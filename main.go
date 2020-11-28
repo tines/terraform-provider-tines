7ddc1058739d79be9eb44f0af6708f4a9d0bfc9d
John Tuckner <tuckner329@gmail.com>
2020-11-28 16:26:41 -0600

    fix provider

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/tuckner/tf-tines/tines"
+	"github.com/tuckner/terraform-provider-tines/tines"
 )
 
 func main() {

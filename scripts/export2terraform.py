1139ec0abd9e99401eed8e06b6e6c9a09203668f
tuckner <tuckner329@gmail.com>
2020-11-30 00:42:11 -0600

    Create export2terraform.py

---
+++ scripts/export2terraform.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json
+
+with open('export.json', 'r') as f:
+    data = json.load(f)
+
+for i, agent in enumerate(data["agents"]):
+    a = """resource "tines_agent" "{0}" {{
+    name = "{0}"
+    agent_type = "{1}"
+    story_id = null
+    keep_events_for = {2}
+    source_ids = []
+    receiver_ids = []
+    agent_options = jsonencode({3})
+}}""".format(agent['name'].replace(' ', '_'), agent['type'], agent['keep_events_for'], json.dumps(agent['options']))
+    print(a)

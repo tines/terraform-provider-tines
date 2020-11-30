3ccdd3d55113847c121573eeb2114547a328d4e0
John Tuckner <tuckner329@gmail.com>
2020-11-29 20:19:16 -0600

    add aws sns example

---
+++ examples/aws_sns/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    tines = {
+      source = "github.com/tuckner/tines"
+      version = "0.0.5"
+    }
+  }
+}
+
+provider "tines" {
+  email    = var.tines_email
+  base_url = var.tines_base_url
+  token    = var.tines_token
+}
+
+provider "aws" {
+  region    = "us-east-1"
+}
+
+
+resource "random_id" "webhook_secret" {
+  byte_length = 8
+}
+
+resource "tines_agent" "webhook" {
+  name = "Webhook Agent"
+  agent_type = "Agents::WebhookAgent"
+  story_id = 31
+  keep_events_for = 604800
+  source_ids = []
+  receiver_ids = []
+  agent_options = jsonencode({
+    "secret": random_id.webhook_secret.dec,
+    "verbs": "get,post"
+  })
+}
+
+resource "tines_agent" "webhook_confirm" {
+  name = "Webhook Confirm"
+  agent_type = "Agents::HTTPRequestAgent"
+  story_id = 31
+  keep_events_for = 604800
+  source_ids = [tines_agent.webhook.aid]
+  receiver_ids = []
+  agent_options = jsonencode({
+    "url": "{{ .webhook_agent.body.SubscribeURL }}",
+    "method": "get"
+  })
+}
+
+resource "aws_sns_topic_subscription" "sns_target" {
+  depends_on = [tines_agent.webhook_confirm]
+  topic_arn = var.aws_topic_arn
+  protocol = "https"
+  endpoint = format("%s/webhook/%s/%s", var.tines_base_url, tines_agent.webhook.guid, random_id.webhook_secret.dec)
+  endpoint_auto_confirms = true
+}
---
+++ examples/aws_sns/variables.tf
@@ -0,0 +1,15 @@
+variable "tines_token" {
+    type = string
+}
+
+variable "tines_email" {
+    type = string
+}
+
+variable "tines_base_url" {
+    type = string
+}
+
+variable "aws_topic_arn" {
+    type = string
+}

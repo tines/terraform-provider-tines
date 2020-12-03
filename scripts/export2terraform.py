9bf6ae35af07a8e026eb115694e49681d2412bcf
tuckner <tuckner329@gmail.com>
2020-12-03 08:23:59 -0600

    Update export2terraform.py

--- scripts/export2terraform.py
+++ scripts/export2terraform.py
@@ -9,7 +9,7 @@ for i, agent in enumerate(data["agents"]):
     a = """resource "tines_agent" "{0}" {{
     name = "{0}"
     agent_type = "{1}"
-    story_id = null
+    story_id = var.story_id
     keep_events_for = {2}
     source_ids = []
     receiver_ids = []

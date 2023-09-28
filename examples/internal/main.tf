355489484a1f5cd68ef3fa7cd7bb536a47c0fc4e
Shayon Mukherjee <shayonj@gmail.com>
2023-09-28 17:00:42 -0400

    Update docs
    Refers to the new naming convention

--- examples/internal/main.tf
+++ examples/internal/main.tf
@@ -15,10 +15,3 @@ resource "tines_story" "dev_story_name" {
   team_id         = var.team_id # optional
   # folder_id       = var.folder_id # optional
 }
-
-# resource "tines-v2_story" "prod_story_name" {
-#   data            = file("${path.module}/prod-story.json")
-#   file_hash       = filesha512("${path.module}/prod-story.json")
-#   tenant_url      = var.prod_tenant_url
-#   tines_api_token = var.prod_tines_api_token
-# }
